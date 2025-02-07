42a2d51a456e88b0a7129256facea9a0b392bf23
Lakshya Kumar <lakshyakumar5023@gmail.com>
2025-02-07 21:41:12 +0530

    used cookies to continued logged in

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants/constants";
+import { setUser } from "../utils/store/userSlice";
 
 const Body = () => {
   const location = useLocation();
+  const dispatch = useDispatch();
+
+  const fetchUsers = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(setUser(res.data));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   const hideNavbar =
     location.pathname.includes("/terms-of-service") ||
     location.pathname.includes("/privacy-policy") ||
