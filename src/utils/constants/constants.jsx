1c63c57324fca09203002fa0b46ab4cf395cb1d1
Lakshya Kumar <lakshyakumar5023@gmail.com>
2025-04-21 14:46:16 +0530

    Update constants.jsx

--- src/utils/constants/constants.jsx
+++ src/utils/constants/constants.jsx
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-sdey.onrender.com/";
+  location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-sdey.onrender.com";
